ec74ba71cd329e57e6c64900a9ace657e586ea9d
Juan Camilo Murcia Ramos <camillomurcia@gmail.com>
2022-05-27 23:02:15 -0500

    Action Execution test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   launch: {
-    headless: false,
+    headless: true,
     args: ['--start-maximized', '--single-process'],
     defaultViewport: null,
     slowMo: 0 //Esto agrega un tiempo de espera por cada interaccion Tiempo en milisegundos
