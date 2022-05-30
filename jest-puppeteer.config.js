ac1f8a4661b23d9acdd42efea3e18a9083acd1e5
Juan Camilo Murcia Ramos <camillomurcia@gmail.com>
2022-05-29 23:43:57 -0500

    Correction for execution

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,38 @@
-module.exports = {
+const ci = Boolean(process.env.CI || false);
+
+const ciPipelineOptions = {
   launch: {
-    headless: false,
-    args: ['--start-maximized', '--single-process'],
+    executablePath: '/usr/bin/google-chrome-stable',
+    headless: true,
+        args: [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--start-maximized',
+      '--single-process'],
     defaultViewport: null,
     slowMo: 0 //Esto agrega un tiempo de espera por cada interaccion Tiempo en milisegundos
   }
 };
+
+const localOption = {
+  launch: {
+    headless: false,
+        args: [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--start-maximized',
+      '--single-process'],
+    defaultViewport: null,
+    slowMo: 0 //Esto agrega un tiempo de espera por cada interaccion Tiempo en milisegundos
+  }
+};
+
+
+module.exports = ci ? ciPipelineOptions : localOption;
+
